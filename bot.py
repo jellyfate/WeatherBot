02d855b5b8171e972913c70af3c4cc309fbc7ac6
amicum <jellyfate@gmail.com>
2019-05-31 18:07:43 +0300

    hourly saving

--- bot.py
+++ bot.py
@@ -7,54 +7,44 @@ This Bot uses the Updater class to handle the bot.
 First, a few handler functions are defined. Then, those functions are passed to
 the Dispatcher and registered at their respective places.
 Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
 """
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging
-import json
-import urllib.request
-import datetime
-import telegram
+import logging, telegram, datetime, sched, time
+from threading import Thread
+import sqlite3 as sql
+from funcs import *
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-locations = {   
-    'еньков': [51.470473, 31.375846], 
-    'кск':    [51.463262, 31.269598],
-}
+# db connection
+try:
+    connection = sql.connect('log.db')
+    cursor = connection.cursor()
+except sql.Error as e:
+    print("Error {}:".format(e.args[0]))
+# finally:
+#     if connection:
+#         connection.close()
 
-current_weather = {   
-    'OpenWeatherMap.org': "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid=b7f629081f4f576144361ffc63ee8fad", 
-    'DarkSky.net':  "https://api.darksky.net/forecast/cfc279055ab44b7e0c7084262d668ca4/{},{}",
-    'APIXU.com': "http://api.apixu.com/v1/current.json?key=3cc653a5a4474dcd828182331193005&q={},{}",
-}
+# hourly saving rain data
+def daily_log(sc): 
+    for location in locations.keys():
+        owm, darksky, apixu = rain(location)
+        # print('INSERT INTO {} VALUES("{}",{},{},{});'.format(
+        #     today_db[location], datetime.date.today(), owm, darksky, apixu))
+        print(datetime.datetime.now())
+        cursor.execute('INSERT INTO {} VALUES("{}",{},{},{});'.format(
+            today_db[location], datetime.datetime.now(), owm, darksky, apixu))
+        connection.commit()
+    scheduler.enter(3600, 1, daily_log, (sc,))
 
-forecast = {   
-    'OpenWeatherMap.org': "https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid=b7f629081f4f576144361ffc63ee8fad", 
-    'DarkSky.net':  "https://api.darksky.net/forecast/cfc279055ab44b7e0c7084262d668ca4/{},{}",
-    'APIXU.com': "http://api.apixu.com/v1/forecast.json?key=3cc653a5a4474dcd828182331193005&q={},{}",
-}
-
-
-def json_to_dict(link):
-     with urllib.request.urlopen( link ) as url:
-        return json.loads(url.read().decode())
-
-def farenhToCelc(farenhT):
-        return round((farenhT - 32) * (5/9), 1) 
-
-def unknown_location_check(location, update):
-    if location not in locations.keys():
-        update.message.reply_text("Не найдено! Доступные локации: " 
-        + ", ".join(list(locations.keys())) + " (чувствительно к регистру!)")
-        return True 
+# sheduler to hourly launch daily_log function
+scheduler = sched.scheduler(time.time, time.sleep)
+scheduler.enter(3600, 1, daily_log, (scheduler,))
 
 
 def start(bot, update): 
@@ -63,6 +53,10 @@ def start(bot, update):
 def help(bot, update):
     update.message.reply_text('Help!')
 
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
 
 def echo(bot, update, args):
     if unknown_location_check(args[0], update): return 
@@ -70,10 +64,6 @@ def echo(bot, update, args):
                
     data = json_to_dict(forecast["DarkSky.net"].format(locations[args[0]][0], locations[args[0]][1]))
     current = data['currently']
-    # data = {}    
-    # for key, value in current_weather.items():
-    #     # take api links, insert into the coordinates taken from location list by key
-    #     data[key] = json_to_dict(value.format(locations[args[0]][0], locations[args[0]][1]))
 
     current['time'] = datetime.datetime.fromtimestamp(current['time'])
     current['temperature'] = str(farenhToCelc(current['temperature'])) + ' C'
@@ -90,37 +80,21 @@ def echo(bot, update, args):
     bot.send_message(chatId, text = reply, parse_mode=telegram.ParseMode.HTML)
 
 
+# send "rain" message
 def rain_now(bot, update, args):
-    if unknown_location_check(args[0], update): return 
-    message = "Местоположение: " + args[0].title() + "\n"
+    if unknown_location_check(args[0], update): return     
+    # get rain data and pack it do dictionary
+    rain_data = dict(zip(current_weather.keys(), rain(args[0])))
 
-    data = {}    
-    for key, value in current_weather.items():
-        # take api links, insert into the coordinates taken from location list by key
-        data[key] = json_to_dict(value.format(locations[args[0]][0], locations[args[0]][1]))
-        
-    # openweather JSON have no "rain" key, when there are no rain in location
-    if "rain" in data["OpenWeatherMap.org"]:
-        own_rain = data["OpenWeatherMap.org"]["rain"]["3h"]
-    else:
-        own_rain = 0    
-
-    template = "{0} : осадки {1} mm \n"
-    message += template.format("OpenWeatherMap.org", own_rain)
-    message += template.format("DarkSky.net", data["DarkSky.net"]["currently"]["precipIntensity"])
-    message += template.format("APIXU.com", data["APIXU.com"]["current"]["precip_mm"])
-    update.message.reply_text(message)
-
-
-def error(bot, update, error):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
+    reply = "Местоположение: " + args[0].title() + "\n"
+    for service in current_weather.keys():
+        reply += "{0} : осадки {1} mm \n".format(service, rain_data[service])
+    update.message.reply_text(reply)
 
 
 def main():
-    """Start the bot."""
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater("812215138:AAENIsKiCloqVsdZlrEvCcPuEUT3OgMSBf8")
+    updater = Updater("765960442:AAFyVQMSztNU4uBwRqBLyOuyMir8W5h3NW4")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -140,11 +114,12 @@ def main():
     # Start the Bot
     updater.start_polling()
 
+    scheduler.run()
+    
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
---
+++ funcs.py
@@ -0,0 +1,32 @@
+import urllib, json
+
+from data import *
+
+
+def json_to_dict(link):
+     with urllib.request.urlopen( link ) as url:
+        return json.loads(url.read().decode())
+
+
+def farenhToCelc(farenhT):
+        return round((farenhT - 32) * (5/9), 1) 
+
+
+def unknown_location_check(location, update):
+    if location not in locations.keys():
+        update.message.reply_text("Не найдено! Доступные локации: " 
+        + ", ".join(list(locations.keys())) + " (чувствительно к регистру!)")
+        return True 
+
+
+# return current rain data in custom location among 3 services
+def rain(location):
+    data = {}    
+    for key, value in current_weather.items():
+        # take api links, insert into the coordinates taken from location list by key
+        data[key] = json_to_dict(value.format(locations[location][0], locations[location][1]))
+    # openweather JSON have no "rain" key, when there are no rain in location
+    own_rain = data["OpenWeatherMap.org"]["rain"]["3h"] if "rain" in data["OpenWeatherMap.org"] else 0  
+    return own_rain, data["DarkSky.net"]["currently"]["precipIntensity"], data["APIXU.com"]["current"]["precip_mm"]
+
+
