3d1f38ea4613c7242aefd85e423316123200cf78
amicum <jellyfate@gmail.com>
2019-06-04 21:54:40 +0300

    format

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """The Bot is created for checking weather in custom location.
@@ -7,8 +7,13 @@ Also this Bot can remind, if at your locations were no rain last few days.
 Then, the bot is started and runs until we press Ctrl-C on the command line.
 """
 
-from telegram.ext import Updater, CommandHandler 
-import logging, telegram, datetime, sqlite3, urllib.request, json
+from telegram.ext import Updater, CommandHandler
+import logging
+import telegram
+import datetime
+import sqlite3
+import urllib.request
+import json
 from data import *
 
 # enable logging
@@ -18,62 +23,74 @@ logger = logging.getLogger(__name__)
 
 # db connection
 try:
-    connection = sqlite3.connect('log.db')    
+    connection = sqlite3.connect('log.db')
 except sqlite3.Error as e:
-    print("Error {}:".format(e.args[0]))   
+    print("Error {}:".format(e.args[0]))
+
 
 def json_to_dict(link):
-     with urllib.request.urlopen( link ) as url:
+    with urllib.request.urlopen(link) as url:
         return json.loads(url.read().decode())
 
+
 def farenhToCelc(farenhT):
-    return round((farenhT - 32) * (5/9), 1) 
+    return round((farenhT - 32) * (5/9), 1)
+
 
 def unknown_location_check(location, update):
     if location not in places.keys():
-        update.message.reply_text("Не найдено! Доступные локации: " 
-        + ", ".join(list(places.keys())) + " (чувствительно к регистру!)")
-        return True 
+        update.message.reply_text("Не найдено! Доступные локации: "
+            + ", ".join(list(places.keys())) + " (чувствительно к регистру!)")
+        return True
 
 
 def current(bot, update, args):
     """Show current weather in custom location."""
-    if unknown_location_check(args[0], update): return 
+    if unknown_location_check(args[0], update):
+        return
     reply = "Местоположение: " + args[0].title() + "\n"
-               
+
     data = json_to_dict(weather["forecast"]["DarkSky.net"]
         .format(places[args[0]]["coordinates"][0], places[args[0]]["coordinates"][1]))
     current = data['currently']
 
     current['time'] = datetime.datetime.fromtimestamp(current['time'])
     current['temperature'] = str(farenhToCelc(current['temperature'])) + ' C'
-    current['apparentTemperature'] = str(farenhToCelc(current['apparentTemperature'])) + ' C'
+    current['apparentTemperature'] = str(
+        farenhToCelc(current['apparentTemperature'])) + ' C'
     current['dewPoint'] = str(farenhToCelc(current['dewPoint'])) + ' C'
     current['humidity'] = str(current['humidity'] * 100) + ' %'
     current['windSpeed'] = str(current['windSpeed']) + ' m/s'
     current['pressure'] = str(current['pressure']) + ' hps'
 
-    for key,val in current.items():
+    for key, val in current.items():
         reply += f"<code>{key}</code> :  {val}\n"
 
     chatId = update.message.chat.id
-    bot.send_message(chatId, text = reply, parse_mode=telegram.ParseMode.HTML)
+    bot.send_message(chatId, text=reply, parse_mode=telegram.ParseMode.HTML)
 
 
 def rain(location):
     """Get current rain data in custom location among 3 services."""
-    data = {}    
+    data = {}
     for key, value in weather["current"].items():
         # take api links, insert into the coordinates taken from location list by key
         data[key] = json_to_dict(value
-        .format(places[location]["coordinates"][0], places[location]["coordinates"][1]))
+            .format(places[location]["coordinates"][0], places[location]["coordinates"][1]))
     # openweather JSON have no "rain" key, when there are no rain in location
-    own_rain = data["OpenWeatherMap.org"]["rain"]["3h"] if "rain" in data["OpenWeatherMap.org"] else 0  
-    return own_rain, data["DarkSky.net"]["currently"]["precipIntensity"], data["APIXU.com"]["current"]["precip_mm"]
+    if "rain" in data["OpenWeatherMap.org"]:
+        own_rain = data["OpenWeatherMap.org"]["rain"]["3h"] 
+    else:
+        own_rain = 0
+    return own_rain, 
+        data["DarkSky.net"]["currently"]["precipIntensity"], 
+        data["APIXU.com"]["current"]["precip_mm"]
+
 
 def rain_now(bot, update, args):
     """Show rain data in custom location."""
-    if unknown_location_check(args[0], update): return     
+    if unknown_location_check(args[0], update):
+        return
     # get rain data and pack it do dictionary
     rain_data = dict(zip(weather["current"].keys(), rain(args[0])))
 
@@ -85,23 +102,26 @@ def rain_now(bot, update, args):
 
 def history(days, location):
     """Get rain log from database in custom location and time range"""
-    connection = sqlite3.connect('log.db') 
+    connection = sqlite3.connect('log.db')
     cursor = connection.cursor()
     # get data from custom location`s database
     cursor.execute("select * from {};".format(places[location]["history_db"]))
-    return cursor.fetchall()[-int(days):] 
+    return cursor.fetchall()[-int(days):]
+
 
 def today(location):
     """Get rain log from database in custom location and time range"""
-    connection = sqlite3.connect('log.db') 
+    connection = sqlite3.connect('log.db')
     cursor = connection.cursor()
     # get data from custom location`s database
     cursor.execute("select * from {};".format(places[location]["today_db"]))
-    return cursor.fetchall() 
+    return cursor.fetchall()
+
 
 def history_and_today(bot, update, args):
     """Show rain log for last week in custom location."""
-    if unknown_location_check(args[0], update): return 
+    if unknown_location_check(args[0], update):
+        return
     # get rain log
     rows = history(7, args[0])
     rows += today(args[0])
@@ -113,76 +133,84 @@ def history_and_today(bot, update, args):
     update.message.reply_text(reply)
 
 
-def hourly_log(bot, job): 
+def hourly_log(bot, job):
     """Write current rain data in today`s database."""
-    connection = sqlite3.connect('log.db') 
+    connection = sqlite3.connect('log.db')
     cursor = connection.cursor()
 
     for location in places.keys():
         # get current rain data
-        owm, darksky, apixu = rain(location)      
+        owm, darksky, apixu = rain(location)
         cursor.execute('INSERT INTO {} VALUES("{}",{},{},{});'.format(
-            places[location]["today_db"], datetime.datetime.now().strftime('today %H:%M'), 
-            "{0:.1f}".format(owm), 
-            "{0:.1f}".format(darksky), 
+            places[location]["today_db"], datetime.datetime.now().strftime(
+                'today %H:%M'),
+            "{0:.1f}".format(owm),
+            "{0:.1f}".format(darksky),
             "{0:.1f}".format(apixu)))
         connection.commit()
 
+
 def set_hourly(job_queue):
     """Shedule sending rain log."""
-    job_queue.run_repeating(hourly_log, interval = 3600, 
-        first = datetime.time(hour = 0, minute = 0)) 
+    job_queue.run_repeating(hourly_log, interval=3600,
+                            first=datetime.time(hour=0, minute=0))
     print('Ежечасные события установлены.')
+
 
 def daily_log(bot, job):
     """Write today rain data in history database."""
-    connection = sqlite3.connect('log.db') 
+    connection = sqlite3.connect('log.db')
     cursor = connection.cursor()
 
     for location in places.keys():
-        cursor.execute("select * from {};".format(places[location]["today_db"]))
-        rows = cursor.fetchall()  
-        # next line needed to get max()      
-        rows = [item for t in rows for item in t]     
+        cursor.execute(
+            "select * from {};".format(places[location]["today_db"]))
+        rows = cursor.fetchall()
+        # next line needed to get max()
+        rows = [item for t in rows for item in t]
         cursor.execute('INSERT INTO {} VALUES("{}",{},{},{});'.format(
-            places[location]["history_db"], datetime.date.today(), 
+            places[location]["history_db"], datetime.date.today(),
             max(rows[1::4]), max(rows[2::4]), max(rows[3::4])))
         # clean the today db because its data transfered to history db
         cursor.execute("delete from {};".format(places[location]["today_db"]))
         connection.commit()
 
+
 def set_daily(job_queue):
     """Shedule sending rain log."""
-    job_queue.run_repeating(daily_log, interval = 86400, 
-        first = datetime.time(hour = 23, minute = 59)) 
+    job_queue.run_repeating(daily_log, interval=86400,
+                            first=datetime.time(hour=23, minute=59))
     print('Eжедневные события установлены.')
 
 
-def reminder(bot, job):  
-    """Send rain log in custom location and time range every day.""" 
+def reminder(bot, job):
+    """Send rain log in custom location and time range every day."""
     # get rain log for last N days
     rows = history(job.context["days"], job.context["location"])
 
-    reply = "Местоположение: " + job.context["location"].title() + "\n"    
+    reply = "Местоположение: " + job.context["location"].title() + "\n"
     template = "{0:10} {1:10} {2:10} {3:10} \n"
     reply += template.format("День", "       OWM", "  DarkSky", "APIXU")
     for row in rows:
         reply += template.format(row[0], row[1], row[2], row[3])
     bot.send_message(chat_id=job.context["chat_id"], text=reply)
 
-def set_reminder(bot, update, job_queue, args):
-    """Shedule sending rain log."""
-    if unknown_location_check(args[0], update): return  
 
-    job_queue.run_repeating(reminder, interval = 86400, 
-        first = datetime.time(hour = 16, minute = 00), 
-        context = {"chat_id" : update.message.chat_id,
-                "days": args[1], "location": args[0]} ) 
+def set_reminder(bot, update, job_queue, args):
+    """Schedule sending rain log."""
+    if unknown_location_check(args[0], update):
+        return
+
+    job_queue.run_repeating(reminder, interval=86400,
+                            first=datetime.time(hour=16, minute=00),
+                            context={"chat_id": update.message.chat_id,
+                                     "days": args[1], "location": args[0]})
     update.message.reply_text('Мониторинг установлен.')
+
 
 def unset_reminder(bot, update, job_queue):
     """Unshedule sending rain log."""
-    if  not all(job_queue.jobs()):
+    if not all(job_queue.jobs()):
         update.message.reply_text('Нет активных мониторингов.')
         return
 
@@ -205,23 +233,23 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("погода", current, pass_args=True))
     dp.add_handler(CommandHandler("дождь", rain_now, pass_args=True))
-    dp.add_handler(CommandHandler("история", history_and_today, pass_args=True))
+    dp.add_handler(CommandHandler(
+        "история", history_and_today, pass_args=True))
     dp.add_handler(CommandHandler("мониторинг", set_reminder,
-                                    pass_args=True,
-                                    pass_job_queue=True,))
-    dp.add_handler(CommandHandler("размониторить", unset_reminder,                                     
-                                    pass_job_queue=True,))
+                                  pass_args=True,
+                                  pass_job_queue=True,))
+    dp.add_handler(CommandHandler("размониторить", unset_reminder,
+                                  pass_job_queue=True,))
     # log all errors
     dp.add_error_handler(error)
 
-    # executing hourly and daily schedules in background
     # start saving to db jobs
     set_daily(job_queue)
     set_hourly(job_queue)
     try:
         # Start the Bot
-        updater.start_polling()  
-        print('Бот работает...') 
+        updater.start_polling()
+        print('Бот работает...')
         # Run the bot until you press Ctrl-C or the process receives SIGINT,
         # SIGTERM or SIGABRT. This should be used most of the time, since
         # start_polling() is non-blocking and will stop the bot gracefully.
@@ -230,6 +258,8 @@ def main():
         # Not strictly necessary if daemonic mode is enabled but should be done if possible
         # scheduler.shutdown()
         connection.close()
+        updater.stop()
+
 
 if __name__ == '__main__':
     main()
